239c031a08c4ecd91b6d99408407aa78e7a7af33
caofangsheng93 <fangsheng93@163.com>
2016-05-28 11:58:42 +0800

    代码提交

--- EF.Core/Data/User.cs
+++ EF.Core/Data/User.cs
@@ -8,12 +8,25 @@ namespace EF.Core.Data
 {
    public class User:BaseEntity
     {
+       /// <summary>
+       /// 用户名
+       /// </summary>
        public string UserName { get; set; }
 
+       /// <summary>
+       /// 电子邮件
+       /// </summary>
        public string Email { get; set; }
 
+       /// <summary>
+       /// 密码
+       /// </summary>
        public string Password { get; set; }
 
-       public UserProfile
+       /// <summary>
+       /// 导航属性--UserProfile【user-UserProfile一对一】
+       /// 注意：这里没有加Virtual关键字，UserProfile实体中，User导航属性加了
+       /// </summary>
+       public UserProfile UserProfile { get; set; }
     }
 }
--- EF.Core/Data/UserProfile.cs
+++ EF.Core/Data/UserProfile.cs
@@ -5,7 +5,26 @@ using System.Text;
 
 namespace EF.Core.Data
 {
-    class UserProfile
+   public class UserProfile:BaseEntity
     {
+       /// <summary>
+       /// 姓氏
+       /// </summary>
+       public string FirstName { get; set; }
+
+       /// <summary>
+       /// 名字
+       /// </summary>
+       public string LastName { get; set; }
+
+       /// <summary>
+       /// 地址
+       /// </summary>
+       public string Address { get; set; }
+
+       /// <summary>
+       /// 导航属性-User
+       /// </summary>
+       public virtual User User { get; set; }
     }
 }
